fda297405ace0e1f5e2c1db6c4d9c9550b1e4991
ashimroyrupok <ashimroyrupok@gmail.com>
2025-01-09 13:57:32 +0600

    fixed pdf uploading issue

--- src/Hooks/UseAxiosPublic.jsx
+++ src/Hooks/UseAxiosPublic.jsx
@@ -4,8 +4,8 @@ const axiosPublic = axios.create({
   // baseURL: 'http://localhost:3000',
   // // baseURL: 'https://student-management-server-production.up.railway.app',
   baseURL:
-    // "https://rpistudentmanagementserver.vercel.app/api/v1",
-    "http://localhost:5000/api/v1",
+    "https://rpistudentmanagementserver.vercel.app/api/v1",
+    // "http://localhost:5000/api/v1",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
